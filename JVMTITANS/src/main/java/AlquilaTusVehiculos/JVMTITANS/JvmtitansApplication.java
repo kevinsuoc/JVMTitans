4346380d554a49ea8f1c9ca56a4f34d7521d4be4
kevin seligmann <kseligmann@uoc.edu>
2025-03-17 19:29:29 +0100

    Init proyecto

---
+++ JVMTITANS/src/main/java/AlquilaTusVehiculos/JVMTITANS/JvmtitansApplication.java
@@ -0,0 +1,13 @@
+package AlquilaTusVehiculos.JVMTITANS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmtitansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmtitansApplication.class, args);
+	}
+
+}
---
+++ JVMTITANS/src/test/java/AlquilaTusVehiculos/JVMTITANS/JvmtitansApplicationTests.java
@@ -0,0 +1,13 @@
+package AlquilaTusVehiculos.JVMTITANS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmtitansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
